50cce44dd3212fdcd82fbc34013fe0c55ec23690
Vaibhav RAJ <141556522+Vaibhavraj604@users.noreply.github.com>
2023-09-02 09:29:09 +0530

    Add files via upload

---
+++ 15 may Assignment (python).py
@@ -0,0 +1,123 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+# Q1. How do you comment code in Python? What are the different types of comments
+
+*In Python Comments Are Identified as Hash Symbol (#)
+    
+*Types Of Comment 
+1= single line comment
+2= Multi Line Comment
+
+
+# In[ ]:
+
+
+# Q2. What are variables in Python? How do you declare and assign values to variables?
+
+*In python variables are simply one type of container that store the data related value.
+
+*The assign operator denoted by the symbol "=" and its work is to assign the value in variable.
+ example=
+    x = 5 (Here is the value is 5 which is assign to variable name as "x" )
+
+
+# In[ ]:
+
+
+# Q3. How do you convert one data type to another in Python?
+
+*There are three type of coversion in python 
+1. list[]
+2. tuple()
+3. set()
+example =
+int(input()) (here input is type casted into integer)
+
+
+# In[ ]:
+
+
+# Q4. How do you write and execute a Python script from the command line?
+
+*The python is a well known high level programming language.the file containg file script it must have the extension (.py)
+example=
+print(HELLO WORLD!)  (For execute this programm type SHIFT+ENTER)
+
+
+# In[8]:
+
+
+# Q5. Given a list my_list = [1, 2, 3, 4, 5], write the code to slice the list and obtain the sub-list [2, 3].
+
+my_list = [1, 2, 3, 4, 5]
+print(my_list[1:3:1])
+
+
+# In[9]:
+
+
+#Q6. What is a complex number in mathematics, and how is it represented in Python?
+
+# complex no in mathematics = 5+6j
+x = 5+6j
+type(x)
+
+
+# In[1]:
+
+
+#Q7. What is the correct way to declare a variable named age and assign the value 25 to it? 
+
+age = int(25)
+print(age)
+
+
+# In[ ]:
+
+
+#Q8. Declare a variable named price and assign the value 9.99 to it. What data type does this variable
+ #belong to?
+    
+price = (9.99)    
+
+
+# In[6]:
+
+
+type(price)
+
+
+# In[1]:
+
+
+#Q9. Create a variable named name and assign your full name to it as a string. How would you print the
+#value of this variable?
+
+name = "vaibhav raj"
+print(name)
+
+
+# In[9]:
+
+
+#Q10. Given the string "Hello, World!", extract the substring "World".
+
+a = "Hello World"
+a[6:11:1]
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
